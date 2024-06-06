6a69426f810ed4e17b23853dbc98d8dbc9a6fe1f
Thibault Dethier <tdethier@bluesquarehub.com>
2024-06-06 14:42:20 +0200

    Added NHIA financial report

--- contribution/report.py
+++ contribution/report.py
@@ -1,5 +1,7 @@
-from contribution.reports import premium_collection, payment_category_overview, contributions_distribution
+from contribution.reports import premium_collection, payment_category_overview, contributions_distribution, \
+    nhia_financial_report
 from contribution.reports.contributions_distribution import contributions_distribution_query
+from contribution.reports.nhia_financial_report import nhia_financial_report_query
 from contribution.reports.payment_category_overview import payment_category_overview_query
 from contribution.reports.premium_collection import premium_collection_query
 
@@ -30,5 +32,14 @@ report_definitions = [
         "module": "contribution",
         "python_query": contributions_distribution_query,
         "permission": ["131206"],
+    },
+    {
+        "name": "nhia_financial_report",
+        "engine": 0,
+        "default_report": nhia_financial_report.template,
+        "description": "NHIA - Financial report",
+        "module": "contribution",
+        "python_query": nhia_financial_report_query,
+        "permission": ["131234"],
     },
 ]
